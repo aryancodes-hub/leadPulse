0b889bd41b8e194433cb211d8cf2265b1c88f715
not-available-aryan <aryan147yadav@gmail.com>
2026-09-24 17:25:29 +0530

    fix vercel standalone bug

--- leadpulse-frontend/next.config.mjs
+++ leadpulse-frontend/next.config.mjs
@@ -5,7 +5,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // output: 'standalone', // Breaks Vercel deployments!
   reactCompiler: true,
   turbopack: {
     root: path.join(__dirname, '../'),
